107b6add72a0e91a0379263dbfb6bb48bc08a010
Ido Perlmuter <ido@ido50.net>
2014-04-11 22:14:00 +0300

    completed first version

--- lib/Homer.pm
+++ lib/Homer.pm
@@ -6,7 +6,6 @@ use warnings;
 use strict;
 
 use Carp;
-use Data::UUID;
 
 our $VERSION = "1.000000";
 $VERSION = eval $VERSION;
@@ -17,11 +16,125 @@ Homer - Simple prototype-based object system
 
 =head1 SYNOPSIS
 
+	use Homer;
+
+	# create a prototype object
+	my $person = Homer->new(
+		first_name => 'Generic',
+		last_name => 'Person',
+		say_hi => sub {
+			my $self = shift;
+			print "Hi, my name is ", $self->first_name, ' ', $self->last_name, "\n";
+		}
+	);
+
+	# create a new object based on it
+	my $homer = $person->extend(
+		first_name => 'Homer',
+		last_name => 'Simpson'
+	);
+
+	$homer->say_hi; # prints 'Hi, my name is Homer Simpson'
+
+	# let's extend even more
+	my $bart = $homer->extend(
+		first_name => 'Bart',
+		father => sub { print "My father's name is ", $_[0]->prot->first_name, "\n" }
+	);
+
+	$bart->say_hi; # prints 'Hi, my name is Bart Simpson'
+	$bart->father; # prints "My father's name is Homer"
+
 =head1 DESCRIPTION
+
+C<Homer> is a very simple B<prototype-based object system>, similar to JavaScript.
+In a prototype based object system there are no classes. Objects are either directly created
+with some attributes and methods, or cloned from existing objects, in which case the object
+being cloned becomes the prototype of the new object. The new object inherits all attributes
+and methods from the prototype. Attributes and methods can be overridden, and new ones can be
+added. The new object can be cloned as well, becoming the prototype of yet another new object,
+thus creating a possibly endless chain of prototypes.
+
+Prototype-based objects can be very powerful and useful in certain cases. They can provide a
+quick way of solving problems. Plus, sometimes you just really need an object, but don't need
+a class. I like to think of prototype-based OO versus class-based OO as being similar to
+schema-less database systems versus relational database systems.
+
+C<Homer> is a quick and dirty implementation of such a system in Perl. As Perl is a class-based
+language, this is merely a hack. When an object is created, C<Homer> creates a specific class just
+for it behind the scenes. When an object is cloned, a new class is created for the clone, with the
+parent object's class pushed to the new one's C<@ISA> variable, thus providing inheritance.
+
+I can't say this implementation is particularly smart or efficient, but it gives me what I need
+and is very lightweight (C<Homer> has no non-core dependencies). If you need a more robust
+solution, L<Class::Prototyped> might fit your need.
+
+=head1 HOMER AT A GLANCE
+
+=over
+
+=item * Prototypes are created by calling C<new()> on the C<Homer> class with a hash, holding
+attributes and methods:
+
+	my $prototype = Homer->new(
+		attr1 => 'value1',
+		attr2 => 'value2',
+		meth1 => sub { print "meth1" }
+	);
+
+	$prototype->attr1; # value1
+	$prototype->attr2; # value2
+	$prototype->meth1; # prints "meth1"
+
+=item * A list of all pure-attributes of an object (i.e. not methods) can be received by
+calling C<attributes()> on the object.
+
+	$prototype->attributes; # ('attr1', 'attr2')
+
+=item * Every object created by Homer can be cloned using C<extend( %attrs )>. The hash can
+contain new attributes and methods, and can override existing ones.
+
+	my $clone = $prototype->extend(
+		attr2 => 'value3',
+		meth2 => sub { print "meth2" }
+	);
+
+	$clone->attr1; # value1
+	$clone->attr2; # value3
+	$clone->meth1; # prints "meth1"
+	$clone->meth2; # prints "meth2"
+
+=item * Objects based on a prototype can refer to their prototype using the C<prot()> method:
+
+	$clone->prot->attr2; # value2
+
+=item * All attributes are read-write:
+
+	$clone->attr1('value4');
+	$clone->attr1; # value4
+	$clone->prot->attr1; # still value1
+
+=item * New methods can be added to an object after its construction. If the object is a
+prototype of other objects, they will immediately receive the new methods too.
+
+	$prototype->add_method('meth3' => sub { print "meth3" });
+	$clone->can('meth3'); # true
+
+=item * New attributes can't be added after construction (for now).
+
+=item * Cloned objects can be cloned too, creating a chain of prototypes:
+
+	my $clone2 = $clone->extend;
+	my $clone3 = $clone2->extend;
+	$clone3->prot->prot->prot; # the original $prototype
+
+=back
 
 =head1 CONSTRUCTOR
 
-=head2 new()
+=head2 new( [ %attrs ] )
+
+Creates a new prototype object with the provided attributes and methods (if any).
 
 =cut
 
@@ -36,8 +149,9 @@ sub new {
 sub _generate_class {
 	my $this_class = shift;
 
-	my $uuid = Data::UUID->new;
-	return $this_class.'::C'.$uuid->create_str;
+	my @caller = caller(1);
+
+	return join('::', $this_class, @caller[3,2]);
 }
 
 sub _generate_object {
@@ -72,7 +186,21 @@ sub _generate_object {
 
 		my $new_class = $this_class->_generate_class;
 		@{"${new_class}::ISA"} = (ref($prot));
+
+		*{"${new_class}::prot"} = sub { $prot };
+
 		return $this_class->_generate_object($new_class, %attrs);
+	};
+
+	*{"${new_class}::add_method"} = sub {
+		my ($self, $name, $code) = @_;
+
+		croak "You must provide the name of the method"
+			unless $name && !ref $name;
+		croak "You must provide an anonymous subroutine"
+			unless $code && ref $code && ref $code eq 'CODE';
+
+		*{"${new_class}::$name"} = $code;
 	};
 
 	return bless \%attrs, $new_class;
@@ -84,13 +212,7 @@ C<Homer> requires no configuration files or environment variables.
 
 =head1 DEPENDENCIES
 
-C<Homer> depends on the following CPAN modules:
-
-=over
-
-=item * Carp
-
-=back
+None other than L<Carp>.
 
 =head1 BUGS AND LIMITATIONS
 
--- t/01-simple.t
+++ t/01-simple.t
@@ -2,7 +2,7 @@
 
 use warnings;
 use strict;
-use Test::More;
+use Test::More tests => 20;
 use Homer;
 
 # let's create a prototype
@@ -14,11 +14,11 @@ ok($person, 'received person object');
 ok($person->can('say_hi'), 'person object can say hi');
 
 # now let's create a real person
-
 my $homer = $person->extend(
 	first_name => 'Homer',
 	last_name => 'Simpson',
 	catch_phrase => "D'Oh!!",
+	say_something_else => sub { "Kids, you tried your best and you failed miserably. The lesson is, never try." }
 );
 
 ok($homer, 'received homer object');
@@ -27,7 +27,6 @@ is($homer->catch_phrase, "D'Oh!!", 'homer object has catch_phrase attribute');
 is($homer->say_hi, 'Hi, my name is Homer Simpson, D\'Oh!!', 'method on homer object called ok');
 
 # now let's extend homer
-
 my $bart = $homer->extend(
 	first_name => 'Bart',
 	catch_phrase => 'Ay Caramba!!',
@@ -35,7 +34,8 @@ my $bart = $homer->extend(
 		my ($self, $one, $two) = @_;
 
 		return ($one - 1) + ($two + 2);
-	}
+	},
+	say_something_else => sub { "My father once said: ".shift->prot->say_something_else }
 );
 
 ok($bart, 'received bart object');
@@ -46,8 +46,22 @@ ok($bart->can('add_numbers'), 'bart can add numbers');
 is($bart->add_numbers(2, 2), 5, 'bart adds numbers correctly (well, not really)');
 ok(!$homer->can('add_numbers'), 'homer can\'t add numbers');
 
+# can we access prototypes?
+is($bart->prot->first_name, 'Homer', 'bart\'s prototype is Homer');
+
 # now let's check some setters
 $homer->first_name('Homer Drunk');
 is($homer->first_name, 'Homer Drunk', 'homer\'s first name changed ok');
+is($bart->catch_phrase('Eat My Shorts'), 'Eat My Shorts', 'bart\'s catch phrase changed ok');
+
+is($bart->say_something_else, "My father once said: Kids, you tried your best and you failed miserably. The lesson is, never try.", 'calling method on prototype works');
+
+# let's see if we can add methods to an existing object
+$homer->add_method('jump' => sub { "no way man" });
+ok($homer->can('jump'), 'homer can jump, maybe');
+
+# can bart also jump now?
+ok($bart->can('jump'), 'bart can jump too');
+is($bart->jump, 'no way man', 'bart doesn\'t want to jump');
 
 done_testing();
